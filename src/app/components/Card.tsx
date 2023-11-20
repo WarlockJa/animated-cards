663fa5a0bcdeaf2ef2c9bfe44c2cb3a206c174fd
warlockja <warlockja@gmail.com>
2023-11-20 12:17:33 +0300

    added mobile version

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,34 +1,39 @@
-import React from "react";
-import test from "../../../public/images/christmas_night_4-wallpaper-1920x1080.jpg";
-
 interface ICard {
-  id: string;
-  title: string;
-  description: string;
-  img: string;
+  item: {
+    id: string;
+    title: string;
+    description: string;
+    img: string;
+  };
   index: number;
+  active: boolean;
+  setActive: (value: number) => void;
 }
-export default function Card({ props }: { props: ICard }) {
-  const { id, index, title, description, img } = props;
+export default function Card(props: ICard) {
+  const { index, active, setActive, item } = props;
   return (
-    <div className="card">
+    <div className={active ? "card card--active" : "card"}>
       <input
         type="radio"
         name="slide"
         className="card--input"
-        id={id}
+        id={item.id}
         defaultChecked
+        onChange={() => setActive(index)}
       />
       <label
-        htmlFor={id}
+        htmlFor={item.id}
         className="card__body"
-        style={{ backgroundImage: `url(${img})` }}
+        style={{
+          backgroundImage: `url(${item.img})`,
+          backgroundPosition: "center",
+        }}
       >
         <div className="card__body__row">
           <div className="card__body__row--icon">{index}</div>
           <div className="card__body__row--description">
-            <h4 className="text">{title}</h4>
-            <p className="text">{description}</p>
+            <h4 className="text">{item.title}</h4>
+            <p className="text">{item.description}</p>
           </div>
         </div>
       </label>
--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,10 +1,39 @@
+"use client";
+import { useState } from "react";
 import Card from "./Card";
 import "./cards.scss";
 import cardsData from "./cardsData.json";
+import useSwipe from "./useSwipe";
 export default function Cards() {
+  const [active, setActive] = useState<number>(cardsData.length);
   const content = cardsData.map((item, index) => (
-    <Card key={item.id} props={{ ...item, index: index + 1 }} />
+    <Card
+      key={item.id}
+      item={item}
+      index={index + 1}
+      active={active === index + 1}
+      setActive={setActive}
+    />
   ));
 
-  return <div className="cardsWrapper">{content}</div>;
+  const handleSwipe = (direction: "left" | "right") => {
+    direction === "left"
+      ? active === cardsData.length
+        ? setActive(1)
+        : setActive(active + 1)
+      : active === 1
+      ? setActive(cardsData.length)
+      : setActive(active - 1);
+  };
+
+  const swipeHandlers = useSwipe({
+    onSwipedLeft: () => handleSwipe("left"),
+    onSwipedRight: () => handleSwipe("right"),
+  });
+
+  return (
+    <div className="cardsWrapper" {...swipeHandlers}>
+      {content}
+    </div>
+  );
 }
